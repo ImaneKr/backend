5c60e885847bd7d4a796789ea7fdaabe2d371f1b
ImaneKr <imanekari1313@gmail.com>
2024-05-11 20:41:12 +0100

    Update guardian.js

--- backend/routes/guardian.js
+++ backend/routes/guardian.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< Updated upstream
 const {
     createGuardian,
     editGuardian,
@@ -7,15 +8,23 @@ const {
     getGuardianById,
     deleteGuardian
 } = require('../controllers/guardian');
+=======
+const { createGuardian, editGuardian, getAllGuardians, getGuardianById } = require('../controllers/guardian');
+>>>>>>> Stashed changes
 
 // Create a new Guardian
 router.post('/', createGuardian);
+<<<<<<< Updated upstream
 
 // Update an existing Guardian
 router.put('/:guardian_id', editGuardian);
 
 // Get all Guardians
+=======
+router.put('/:id', editGuardian);
+>>>>>>> Stashed changes
 router.get('/', getAllGuardians);
+router.get('/:id', getGuardianById);
 
 // Get a Guardian by ID
 router.get('/:guardian_id', getGuardianById);
