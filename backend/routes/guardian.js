285c6716258a6060cd31e1fe301d2c90de1adf04
ImaneKr <imanekari1313@gmail.com>
2024-05-11 16:23:28 +0100

    up11

--- backend/routes/guardian.js
+++ backend/routes/guardian.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const { createGuardian, editGuardian, getAllGuardians } = require('../controllers/guardian');
+const {
+    createGuardian,
+    editGuardian,
+    getAllGuardians,
+    getGuardianById,
+    deleteGuardian
+} = require('../controllers/guardian');
 
+// Create a new Guardian
 router.post('/', createGuardian);
 
+// Update an existing Guardian
 router.put('/:guardian_id', editGuardian);
 
+// Get all Guardians
 router.get('/', getAllGuardians);
+
+// Get a Guardian by ID
+router.get('/:guardian_id', getGuardianById);
+
+// Delete a Guardian
+router.delete('/:guardian_id', deleteGuardian);
 
 module.exports = router;
